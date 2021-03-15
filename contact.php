41db8a3eee12191b4b48fd4c7029cd3b9172668e
AbisaX <73003791+AbisaX@users.noreply.github.com>
2021-03-15 18:14:28 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,14 @@
+<? php 
+$nombre = $_POST['nombre'];
+$asunto = $_POST['asunto'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+$header ='From: Abisaid <abisaidgc@ufps.edu.co>' . "\r\n" .
+'Reply-To: abisaidgc@ufps.edu.co ' . "\r\n" .
+'Content-Type: text/plain; charset=utf-8' . "\r\n" . 
+'X-Mailer: PHP/' . phpversion();
+
+mail($correo,$asunto,$mensaje,$header);
+
+?>
